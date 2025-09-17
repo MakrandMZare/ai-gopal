05e4afcbf167066df0fcb5faebb5e840b83d4311
MakrandMZare, Senior Software Engineer & DevOps Architect <makzare1463@gmail.com>
2025-09-17 14:28:22 +0530

    Backend Update

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import cors from "cors";
 import express from "express";
 import mariadb from "mariadb";
+import ticketRoutes from "./routes/ticket.js";
 import userRoutes from "./routes/user.js";
+import { serve } from "inngest/express";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +16,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
 
 mariadb
   .createConnection(process.env.MARIADB_URI)
